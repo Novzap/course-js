a81213f3c2f4e89a2f16c47ac00308c61156f04f
Novzap <ONKAROSSA@yandex.ru>
2019-04-20 22:27:12 +0300

    lesson 2 fixed

--- lesson 2/main.js
+++ lesson 2/main.js
@@ -12,50 +12,57 @@
         income: [],
         savings: false
     };
-    let costItem = prompt("Введите обязательную статью расходов в этом месяце", "");
-    let sum = +prompt("Во сколько обойдётся?", "");
-    let costItem2 = prompt("Введите обязательную статью расходов в этом месяце", "");
-    let sum2 = +prompt("Во сколько обойдётся?", "");
 
-    for(let i = 0; i < 2; i++) {
+    for(let i = 0; i < 2;) {
         let costItem = prompt("Введите обязательную статью расходов в этом месяце", "");
         let sum = +prompt("Во сколько обойдётся?", "");
-        if( (typeof(a)) === 'string' && (typeof(a)) !=null && (typeof(b)) !=null && a != '' && b != '' && a.length < 50) {
+        if( (typeof(costItem)) === 'string' && (typeof(costItem)) !=null && (typeof(sum)) !=null && costItem != '' && sum != '' && costItem.length < 50) {
             console.log("done");
             appData.expenses[costItem] = sum;
+            i++;
         } else {
-        console.log("Что-то не то") 
+            alert("Введите корректные данные")
         }
     }
     // let count = 2;
     // while(count) {
     //     let costItem = prompt("Введите обязательную статью расходов в этом месяце", "");
     //     let sum = +prompt("Во сколько обойдётся?", "");
-    //     if( (typeof(a)) === 'string' && (typeof(a)) !=null && (typeof(b)) !=null && a != '' && b != '' && a.length < 50) {
+    //     if( (typeof(costItem)) === 'string' && (typeof(costItem)) !=null && (typeof(sum)) !=null && costItem != '' && sum != '' && costItem.length < 50) {
     //         console.log("done");
     //         appData.expenses[costItem] = sum;
+    //         count--;
     //     } else {
-    //     console.log("Что-то не то") 
+    //          console.log("Введите корректные данные");
     //     }
-    //     count--;
     // }
 
     // let count2 = 2;
     // do {
     //     let costItem = prompt("Введите обязательную статью расходов в этом месяце", "");
     //     let sum = +prompt("Во сколько обойдётся?", "");
-    //     if( (typeof(a)) === 'string' && (typeof(a)) !=null && (typeof(b)) !=null && a != '' && b != '' && a.length < 50) {
+    //     if( (typeof(costItem)) === 'string' && (typeof(costItem)) !=null && (typeof(sum)) !=null && costItem != '' && sum != '' && costItem.length < 50) {
     //         console.log("done");
     //         appData.expenses[costItem] = sum;
+    //         count2--;
     //     } else {
-    //     console.log("Что-то не то") 
+    //     console.log("Введите корректные данные"); 
     //     }
-    //     count2--;
     // } while(count2);
 
 
     appData.moneyPerDay = appData.userBudget / 30;
 
     alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+    if(appData.moneyPerDay < 100) {
+        console.log("Минимальный уровень достатка");
+    } else if (appData.moneyPerDay > 100 & appData.moneyPerDay < 2000) {
+        console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("Высокий уровень достатка");
+    } else {
+        console.log("Ошибка");
+    }
     console.log(appData); // Вывод объекта в консоль для проверки
 })();
