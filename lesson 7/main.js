45a72000fecfdbb7486b3034a2018401900f55a3
Novzap <ONKAROSSA@yandex.ru>
2019-05-07 23:11:19 +0300

    fixed lesson 7

--- lesson 7/main.js
+++ lesson 7/main.js
@@ -1,11 +1,10 @@
-let div = document.querySelector('div');
-
-
 function getTimeRemaining() {
-    let t = (Date.parse(new Date()) + ((new Date().getTimezoneOffset()) * 60 * 1000)),
-        seconds = Math.floor((t/1000) % 60),
-        minutes = Math.floor((t/1000/60) % 60),
-        hours = Math.floor((t/(1000*60*60)));
+    let t = new Date(),
+        seconds = t.getSeconds(),
+        minutes = t.getMinutes(),
+        hours = t.getHours();
+        console.log(hours);
+        console.log(t);
     return {
         'total': t,
         'hours': hours,
@@ -19,32 +18,22 @@ function setClock(id) {
     let timer = document.getElementById(id),
         hours = timer.querySelector('.hours'),
         minutes = timer.querySelector('.minutes'),
-        seconds = timer.querySelector('.seconds'),
-        timeInterval = setInterval(updateClock, 1000);
+        seconds = timer.querySelector('.seconds');
+        setInterval(updateClock, 1000);
     function updateClock() {
         let t = getTimeRemaining();
-        hours.textContent = t.hours;
-        minutes.textContent = t.minutes;
-        seconds.textContent = t.seconds;
-
-        if(t.total <= 0) {
-            clearInterval(timeInterval);
-            hours.textContent = '00',
-            minutes.textContent = '00',
-            seconds.textContent = '00';
-        }
+        checkTime(hours, t.hours);
+        checkTime(minutes, t.minutes);
+        checkTime(seconds, t.seconds);
+        
+    }
+}
+function checkTime(t, point) {
+    if(point < 10) {
+        t.textContent = '0' + point;
+    } else {
+        t.textContent = point;
     }
 }
 
 setClock('timer');
-
-function changeDate(date) {
-    if(date < 10) {
-        date = '0' + date;
-        return date;
-    } else {
-        return date;
-    }
-}
-
-div.textContent = changeDate(date.getHours()) + ':' + changeDate(date.getMinutes()) + ':' + changeDate(date.getSeconds());
